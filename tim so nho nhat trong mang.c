5ab7ef2e6631d3ee33494ec99dc04ef5bf99df8a
NorSaH77 <167691649+NorSaH77@users.noreply.github.com>
2024-04-28 01:20:40 +0700

    tim so nho nhat trong mang.c

--- tim so nho nhat trong mang.c
+++ tim so nho nhat trong mang.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 
-int main(){
-int mang[] = { 25 ,21 , 77 , 12 ,15 ,16 ,22 };
-int do_dai = sizeof ( mang ) / sizeof ( mang[0] );
-int nho_nhat = mang[0];
-for ( int i = 0 ; i < do_dai ; i++ ){
-if ( mang[i] < nho_nhat ){
-nho_nhat = mang[i]; }
-} 
-printf(" %d " , nho_nhat );
-return 0;
+int main() {
+    int mang[] = { 25,21, 77, 12,15,16,22 };
+    int do_dai = sizeof ( mang ) / sizeof ( mang[0] );
+    int nho_nhat = mang[0];
+    for ( int i = 0 ; i < do_dai ; i++ ) {
+        if ( mang[i] < nho_nhat ) {
+            nho_nhat = mang[i];
+        }
+    }
+    printf(" %d ", nho_nhat );
+    return 0;
 }
