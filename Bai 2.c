fe891ad1eb9fee80a8769dd9ab8f4e2675c88095
NorSaH77 <nguyenvhoang0707@gmail.com>
2024-04-27 17:48:35 +0700

    Create Bai 2.c

---
+++ Bai 2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int a ;
+    printf(" Nhap so nguyen bat ki : ");
+    scanf("%d", &a);
+    ( a % 2 == 0) ? printf("\n Day la so chan ") : printf("\n Day la so le ");
+    return 0;
+}
