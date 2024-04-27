7b9ff35472e1429c6fe8e2f12a1f4dab11c899a7
NorSaH77 <167691649+NorSaH77@users.noreply.github.com>
2024-04-27 23:56:18 +0700

    Update

---
+++ tim so nho nhat trong mang.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+int mang[] = { 25 ,21 , 77 , 12 ,15 ,16 ,22 };
+int do_dai = sizeof ( mang ) / sizeof ( mang[0] );
+int nho_nhat = mang[0];
+for ( int i = 0 ; i < do_dai ; i++ ){
+if ( mang[i] < nho_nhat ){
+nho_nhat = mang[i]; }
+} 
+printf(" %d " , nho_nhat );
+return 0;
+}
