6151387205083765ec372208398a2e1930467c83
NorSaH77 <167691649+NorSaH77@users.noreply.github.com>
2024-05-04 20:18:09 +0700

    Update

---
+++ bai 8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    int a,b;
+    printf("nhap he so a = ");
+    scanf("%d",&a);
+    printf("\nnhap he so b = ");
+    scanf("%d",&b);
+    printf("\nphuong trinh sau khi nhap la : %d*x+%d=0",a,b);
+    if(a!=0) {
+        printf("\nphuong trinh co nghiem duy nhat x=-%d/%d",b,a);
+    } else if(b!=0) {
+        printf("\nphuong trinh vo nghiem");
+    } else {
+        printf("\nnghiem dung voi moi x");
+    }
+    return 0;
+}
+
