6e2c1eff6f49022245bb483436e406ae4a500145
NorSaH77 <167691649+NorSaH77@users.noreply.github.com>
2024-05-06 11:34:59 +0700

    Update bai 29.c

--- bai 29.c
+++ bai 29.c
@@ -7,8 +7,10 @@ int main(){
     A = 0;
     printf("nhap so nguyen x : ");
     scanf("%d",&x);
+    do{
     printf("\nnhap so nguyen n : ");
     scanf("%d",&n);
+    }while(n<1);
     for(i=1;i<=n;i++){
         A+=(float)pow((-1),(i+1))*x/(10*i);
     }
