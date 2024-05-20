f1ccc8d1760fc0700da62cedcee466b76def68f3
NorSaH77 <167691649+NorSaH77@users.noreply.github.com>
2024-05-20 12:58:46 +0700

    Update lua chon bai muon lam.c

--- lua chon bai muon lam.c
+++ lua chon bai muon lam.c
@@ -45,14 +45,29 @@ void bai3(){
     printf("gia tri cua bieu thuc T la : %.2f",A);
 }
 
+void bai4(){
+    int a=50000,b=20000,c=10000,x,y,z;
+    for(x=0;x<=4;x++){
+        for(y=0;y<=10;y++)
+        for(z=0;z<=20;z++)
+        if(a*x+b*y+c*z==200000)
+        printf("\ncan %d to %dvnd, %d to %dvnd, %d to %dvnd de co so tien 200000vnd",x,a,y,b,z,c);
+    }
+}
+
 int main(){
     int n;
     printf("Chon bai muon lam :\n");
     printf("1.Nhap vao so n<100.Kiem tra xem n co phai so hoan hao hay khong.\n");
     printf("2.Nhap so nguyen n.Kiem tra xem n co phai so nguyen to khong.\n");
     printf("3.Nhap 2 so nguyen n,x.Tinh bieu thuc T=10+2/x+3/x+...+n/x.\n");
+    printf("4.Tim cac kha nang de co to 200k can bao nhieu to 10k,20k,50k.\n");
+    do{
     printf("Nhap so bai muon lam : ");
     scanf("%d",&n);
+    if(n>=5)
+    printf("Vui long nhap lai!\n");
+    }while(n>=5);
     switch (n){
         case 1:
         bai1();
@@ -62,6 +77,9 @@ int main(){
         break;
         case 3:
         bai3();
+        break;
+        case 4:
+        bai4();
         break;
     }
     return getch();
